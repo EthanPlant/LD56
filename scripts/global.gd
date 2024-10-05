9d940c54df2fc3f52af36f27a56be247f0e4b80e
Ethan Plant <plant.ethan@gmail.com>
2024-10-05 09:36:37 -0400

    Speed up minigames after each 5

--- scripts/global.gd
+++ scripts/global.gd
@@ -1,3 +1,5 @@
 extends Node
 
+const SPEED_ITER = 0.25
+const SPEED_MAX = 2.0
 var speed_mult = 1.0
--- scripts/minigames/minigame_player.gd
+++ scripts/minigames/minigame_player.gd
@@ -2,11 +2,19 @@ extends Node2D
 
 var _minigames = ["feed_cats", "clean_litter", "pet_cat", "play_with"]
 var _current_game
+var _games_played
 
 func _ready() -> void:
+	_games_played = -1
 	_switch_game()
 
 func _switch_game():
+	_games_played += 1
+	if _games_played > 0 and _games_played % 5 == 0:
+		Global.speed_mult += Global.SPEED_ITER
+		if Global.speed_mult >= Global.SPEED_MAX:
+			Global.speed_mult = Global.SPEED_MAX
+			
 	var game = _minigames[randi() % _minigames.size()]
 	var path = "res://scenes/minigames/%s/%s.tscn" % [game, game]
 	var game_scene = load(path).instantiate()
@@ -15,7 +23,7 @@ func _switch_game():
 	game_scene.game_won.connect(_on_minigame_win)
 	game_scene.game_lost.connect(_on_minigame_loss)
 	_current_game = game_scene
-
+	
 func _on_minigame_win():
 	_current_game.queue_free()
 	_switch_game()
