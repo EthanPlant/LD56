c74f44b2a21d7827e52850d6c268b516a100f4a2
Ethan Plant <plant.ethan@gmail.com>
2024-10-05 15:25:24 -0400

    Art for catching food

--- scripts/minigames/feed_cats/food_bowl.gd
+++ scripts/minigames/feed_cats/food_bowl.gd
@@ -5,4 +5,4 @@ func _ready() -> void:
 
 func _process(_delta: float) -> void:
     var mouse_position = get_global_mouse_position()
-    position.x = clamp(mouse_position.x, 0, get_viewport().size.x - 750)
+    position.x = clamp(mouse_position.x, $Sprite2D.texture.get_width() / 2, get_viewport_rect().size.x - $Sprite2D.texture.get_width() / 2)
